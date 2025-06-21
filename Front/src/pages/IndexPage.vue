46eb85d52a0e2ec158dfe72f2eaf293c66c803c9
JrAdilson <imjrgonc@gmail.com>
2025-06-21 14:01:41 -0300

    feat: using pgadmin on localhost

--- Front/src/pages/IndexPage.vue
+++ Front/src/pages/IndexPage.vue
@@ -7,6 +7,12 @@
           <q-btn color="secondary" round dense flat icon="delete" @click="deleteParticipant(props.row.id)" />
         </q-td>
       </template>
+
+      <template v-slot:top>
+        <span>Funcionários</span>
+        <q-space />
+        <q-btn color="secondary" round dense flat icon="add" @click="addParticipant()" />
+      </template>
     </q-table>
   </q-page>
 </template>
@@ -15,12 +21,13 @@
 import participantsService from 'src/services/participants';  
 import { defineComponent, onMounted, ref } from 'vue';
 import { useQuasar } from 'quasar';
+import ParticipantDialog from 'src/pages/ParticipantDialog.vue';
 
 export default defineComponent({
   name: 'IndexPage',
   setup() {
     const participants = ref([]);
-    const { get } = participantsService();
+    const { get, post, put, remove } = participantsService();
     const $q = useQuasar();
     const columns = [
       { name: 'name', label: 'Nome', field: 'name', sortable: true, align: 'left' },
@@ -36,46 +43,126 @@ export default defineComponent({
     });
 
     const getParticipants = async () => {
-      const data = await get();
-      participants.value = data.participants
+      try {
+        const data = await get();
+        participants.value = data.participants
+      } catch (error) {
+        $q.notify({ message: `Erro ao carregar funcionários! ${error}`, icon: 'close', color: 'negative', position: 'top' });
+      }
     }
 
-    const deleteParticipant = async (id) => {
+    const deleteParticipant = (id) => {
       try {
-        $q.dialog({
-          title: 'Confirmação',
-          message: 'Você tem certeza que deseja remover este funcionário?',
-          persistent: true,
-          ok: {
-            label: 'Sim',
-            color: 'secondary'
-          },
-          cancel: {
-            label: 'Não',
-            color: 'secondary'
-          }
-        }).onOk(async () => {
-          await participantsService().remove(id);
-          $q.notify({ message: 'Funcionário removido com sucesso!', icon: 'check', color: 'positive', position: 'top' });
-          getParticipants();
-        }).onCancel(() => {
-          $q.notify({ message: 'Operação cancelada!', icon: 'close', color: 'negative', position: 'top' });
-        });
+        dialogParticipant({ id }, 'remove');
       } catch (error) {
         $q.notify({ message: `Erro ao remover funcionário! ${error}`, icon: 'close', color: 'negative', position: 'top' });
         return;
       }
     }
 
-    const editParticipant = async (participant) => {
-      await participantsService().put(participant);
-      getParticipants();
+    const addParticipant = () => {
+      dialogParticipant(
+        {
+          name: '',
+          cpf: '',
+          email: '',
+          shirt_size: '',
+          shoe_size: ''
+        }, 
+        'new'
+      );
     }
+
+    // @TO-DO: validate participant (cpf, email, size shirt/shoe)
+    /*const validateParticipant = async (participant) => {
+      await participantsService().validate(participant)
+        .then(() => {
+          $q.notify({ message: 'Funcionário validado com sucesso!', icon: 'check', color: 'positive', position: 'top' });
+        })
+        .catch((error) => {
+          $q.notify({ message: `Erro de validação: ${error}`, icon: 'close', color: 'negative', position: 'top' });
+          throw new Error(error);
+        });
+    }*/
+
+    const editParticipant = (participant) => {
+      dialogParticipant(participant, 'new');
+    }
+
+    const dialogParticipant = (participant, action) => {
+      const defaultOptions = { 
+          component: ParticipantDialog,
+          componentProps: { 
+            data: participant,
+            title: 'Adicionar Funcionário'
+          },
+      };
+
+      const obj = { 
+        'new': {
+          method: post,
+          title: 'Adicionar',
+          verb: 'adicionado',
+          options: { ...defaultOptions },
+        },
+        'edit': {
+          method: put,
+          title: 'Editar',
+          verb: 'editado',
+          options: { ...defaultOptions },
+        },
+        'remove': {
+          method: remove,
+          options: {
+            message: 'Você tem certeza que deseja remover este funcionário?',
+          },
+          title: 'Remover',
+          verb: 'removido'
+        }
+      };
+      
+      const type = obj[action];
+      if (!type) {
+        $q.notify({ message: 'Ação inválida!', icon: 'close', color: 'secondary', position: 'top' });
+        return;
+      }
+
+      const options = {
+        title: type.title,
+        persistent: true,
+        ok: {
+          label: 'Sim',
+          color: 'secondary'
+        },
+        cancel: {
+          label: 'Não',
+          color: 'secondary'
+        }
+      }
+      
+      $q.dialog({ ...options, ...type.options }).onOk(async (data) => {
+        try {
+          const payload = action === 'remove'
+            ? participant.id
+            : (data?.participant || data);
+
+          await type.method(payload);
+          $q.notify({ message: `Funcionário ${type.verb} com sucesso!`, icon: 'check', color: 'positive', position: 'top' });
+          getParticipants();
+        } catch (error) {
+          $q.notify({ message: `Erro ao ${type.title.toLowerCase()} funcionário! ${error}`, icon: 'close', color: 'negative', position: 'top' });
+        }
+      }).onCancel(() => {
+        $q.notify({ message: 'Operação cancelada!', icon: 'close', color: 'negative', position: 'top' });
+      });
+    };
 
     return {
       participants,
       columns,
+      addParticipant,
       deleteParticipant,
+      dialogParticipant,
       editParticipant
     }
   }
